a73b1a596ac7a853875c0e8e5e91b2eb1b52e239
monxero <priquelme.o89@gmail.com>
2026-08-06 13:32:30 -0400

    Extraer macros (calorías, proteína, carbohidratos, grasa, fibra) de un alimento USDA

--- src/NutriInsights.Importador/Program.cs
+++ src/NutriInsights.Importador/Program.cs
@@ -12,7 +12,19 @@ var sandia = archivo.FoundationFoods.First(a => a is not null && a.FdcId == 2747
 
 Console.WriteLine($"Nombre: {sandia.Description}");
 Console.WriteLine($"Categoría: {sandia.FoodCategory?.Description}");
-foreach (var nutriente in sandia.FoodNutrients)
+var macros = ExtraerMacros(sandia);
+Console.WriteLine($"Calorías: {macros.Calorias}, Proteína: {macros.Proteina}, Carbohidratos: {macros.Carbohidratos}, Grasa: {macros.Grasa}, Fibra: {macros.Fibra}");
+
+static (decimal? Calorias, decimal? Proteina, decimal? Carbohidratos, decimal? Grasa, decimal? Fibra) ExtraerMacros(AlimentoUsdaDto alimento)
 {
-    Console.WriteLine($"  {nutriente.Nutrient.Number} - {nutriente.Nutrient.Name}: {nutriente.Amount} {nutriente.Nutrient.UnitName}");
+    decimal? BuscarPorNumero(params string[] numeros) =>
+        alimento.FoodNutrients.FirstOrDefault(n => numeros.Contains(n.Nutrient.Number))?.Amount;
+
+    return (
+        Calorias: BuscarPorNumero("208", "957"),
+        Proteina: BuscarPorNumero("203"),
+        Carbohidratos: BuscarPorNumero("205"),
+        Grasa: BuscarPorNumero("204"),
+        Fibra: BuscarPorNumero("291")
+    );
 }
