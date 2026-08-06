d56a93d8c475d1c4d1f648ed4ea2da637f45aa13
monxero <priquelme.o89@gmail.com>
2026-08-06 13:01:25 -0400

    Conectar ApplicationDbContext en el importador, verificado contra la base real

--- src/NutriInsights.Importador/Program.cs
+++ src/NutriInsights.Importador/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using NutriInsights.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+
+
+var configuracion = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var cadenaConexion = configuracion.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(cadenaConexion))
+{
+    Console.WriteLine("No se encontró la cadena de conexión.");
+}
+else
+{
+    Console.WriteLine($"Cadena de conexión encontrada, largo: {cadenaConexion.Length} caracteres.");
+}
+
+var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+    .UseNpgsql(cadenaConexion)
+    .Options;
+
+using var db = new ApplicationDbContext(opciones);
+
+var cantidadCategorias = db.CategoriasAlimento.Count();
+Console.WriteLine($"Categorías encontradas en la base: {cantidadCategorias}");
